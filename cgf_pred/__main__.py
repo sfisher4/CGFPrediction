98a8369871dd3416b11bad2a2b78c97df9237e71
sfisher <shannah.elizabeth@gmail.com>
2017-09-08 10:49:14 -0600

    removed embedded fastas

--- cgf_pred/__main__.py
+++ cgf_pred/__main__.py
@@ -7,8 +7,8 @@ from glob import glob
 
 def main(args=None):
 
-    # forward_primers = "/home/sfisher/Sequences/cgf_forward_primers.fasta"  # fasta file with primer id's and primer sequences
-    # reverse_primers = "/home/sfisher/Sequences/cgf_reverse_primers.fasta"  # fasta file with primer id's and primer sequences
+    forward_primers = "/home/sfisher/Sequences/cgf_forward_primers.fasta"  # fasta file with primer id's and primer sequences
+    reverse_primers = "/home/sfisher/Sequences/cgf_reverse_primers.fasta"  # fasta file with primer id's and primer sequences
     amplicon_sequences = "/home/sfisher/Sequences/amplicon_sequences/amplicon_sequences.fasta"
     all_genomes = "/home/sfisher/Sequences/11168_test_files/246_gnomes_2nd_tests"
 
@@ -20,7 +20,8 @@ def main(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("genomes", help="a path to a directory that contains fasta files for all genomes of interest", type=str)
     args=parser.parse_args()
-    CGFPrediction.main(args.genomes, all_fasta_files[1], all_fasta_files[2], amplicon_sequences)
+    # CGFPrediction.main(args.genomes, all_fasta_files[1], all_fasta_files[2], amplicon_sequences)
+    CGFPrediction.main(args.genomes, forward_primers, reverse_primers, amplicon_sequences)
 
 if __name__ == '__main__':
     main()
