af1890cd12cc4f968854689f2add5a26cb6b1f16
sfisher <shannah.elizabeth@gmail.com>
2017-09-08 14:06:13 -0600

    paths

--- cgf_pred/__main__.py
+++ cgf_pred/__main__.py
@@ -4,7 +4,7 @@ from cgf_pred import CGFPrediction
 import pkg_resources
 from glob import glob
 import subprocess
-
+from pathlib import Path
 
 def main(args=None):
 
@@ -14,17 +14,25 @@ def main(args=None):
     all_genomes = "/home/sfisher/Sequences/11168_test_files/246_gnomes_2nd_tests"
 
     DATA_PATH = pkg_resources.resource_filename('cgf_pred', 'fastas/')
-    DB_FILE_AMP = pkg_resources.resource_filename('cgf_pred', 'fastas/amp_seq.fasta')
-    DB_FILE_FP = pkg_resources.resource_filename('cgf_pred', 'fastas/f_primers.fasta')
-    DB_FILE_RP = pkg_resources.resource_filename('cgf_pred', 'fastas/r_primers.fasta')
+    # DB_FILE_AMP = pkg_resources.resource_filename('cgf_pred', 'fastas/amp_seq.fasta')
+    # DB_FILE_FP = pkg_resources.resource_filename('cgf_pred', 'fastas/f_primers.fasta')
+    # DB_FILE_RP = pkg_resources.resource_filename('cgf_pred', 'fastas/r_primers.fasta')
+    print(type(DATA_PATH))
     # all_fasta_files = glob(DATA_PATH + '*.fasta')
+    paths = {fasta.stem: pkg_resources.resource_filename('cgf_pred', fasta)
+             for fasta in Path(DATA_PATH).glob('*.fasta')}
+    print(paths)
+    for path in paths:
+        print(path)
 
     if args is None:
         args = sys.argv[1: ]
     parser = argparse.ArgumentParser()
-    parser.add_argument("genomes", help="a path to a directory that contains fasta files for all genomes of interest", type=str)
+    parser.add_argument("genomes",
+                        help="a path to a directory that contains fasta files for all genomes of interest",
+                        type=str)
     args=parser.parse_args()
-    CGFPrediction.main(args.genomes, DB_FILE_FP, DB_FILE_RP, DB_FILE_AMP)
+    CGFPrediction.main(args.genomes, paths[1], paths[2], paths[0])
     # CGFPrediction.main(args.genomes, all_fasta_files[1], all_fasta_files[2], all_fasta_files[0])
 
 if __name__ == '__main__':
