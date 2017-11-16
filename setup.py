cd83678ad679bbb6c49c18f54712618222d9dd35
sfisher <shannah.elizabeth@gmail.com>
2017-11-16 10:48:38 -0700

    added csv files to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version="0.1",
     packages=find_packages() + ['cgf_pred'],
     package_dir={'cgf_pred': 'cgf_pred'},
-    package_data={'cgf_pred': ['fastas/*.fasta']},
+    package_data={
+        'cgf_pred': ['fastas/*.fasta', 'csvs/*.txt']
+    },
     entry_points={
         'console_scripts': [
             'eCGF = cgf_pred.__main__:main'
