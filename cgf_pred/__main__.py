fac8dc668a4fd6ac3078098a63dbeac0bf2bc51a
sfisher <shannah.elizabeth@gmail.com>
2017-11-16 11:32:56 -0700

    Main fixed from args None

--- cgf_pred/__main__.py
+++ cgf_pred/__main__.py
@@ -6,7 +6,20 @@ from glob import glob
 import subprocess
 from pathlib import Path
 
-def main(args=None):
+def arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('genomes',
+                        help="a path to a directory containing fasta files for all query genomes",
+                        type=str)
+
+    parser.add_argument('out_file',
+                        help="a path to a csv file where eCGF results should be printed.",
+                        type=str)
+
+    return parser.parse_args()
+
+def main():
 
     DATA_PATH = pkg_resources.resource_filename('cgf_pred', 'fastas/')
     paths = {fasta.stem: pkg_resources.resource_filename('cgf_pred', 'fastas/' + str(fasta.stem) + '.fasta')
@@ -16,18 +29,7 @@ def main(args=None):
     db_paths = {txt.stem: pkg_resources.resource_filename('cgf_pred', 'csvs/' + str(txt.stem) + '.txt')
                 for txt in Path(DB_PATH).glob('*.txt')}
 
-    if args is None:
-        args = sys.argv[1: ]
-    parser = argparse.ArgumentParser()
-    parser.add_argument('genomes',
-                        help="a path to a directory containing fasta files for all query genomes",
-                        type=str)
-    parser.add_argument('out_file',
-                       help = "a path to a csv file where eCGF results should be printed.",
-                       type=str)
-
-    #TODO: add optional argument for validation
-    args = parser.parse_args()
+    args = arguments()
     CGFPrediction.main(args.genomes, args.out_file, paths['f_primers'], paths['r_primers'], paths['amp_seq'],
                        paths['cj0181_f_primer'], paths['cj0181_r_primer'], db_paths['error_rate'])
 
